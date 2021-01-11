a31b79847a1e9d896e98aa92f6e7e1431259f39c
sxml <59968675@qq.com>
2021-01-11 11:22:17 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,10 +12,19 @@
 #移除不用软件包    
 rm -rf package/lean/luci-app-dockerman
 rm -rf package/lean/luci-app-wrtbwmon
+rm -rf package/lean/luci-app-accesscontrol
+rm -rf package/lean/luci-app-rclone
+rm -rf package/lean/luci-app-unblockmusic
+rm -rf package/lean/luci-app-vlmcsd
+rm -rf package/lean/luci-app-vsftpd
+rm -rf package/lean/luci-app-wol
+rm -rf package/lean/luci-app-udpxy
+
 #添加额外软件包
 #git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
+
 # 编译 po2lmo (如果有po2lmo可跳过)
 #pushd package/luci-app-openclash/tools/po2lmo
 #make && sudo make install
@@ -52,12 +61,13 @@ svn co https://github.com/rosywrt/luci-theme-rosy/trunk/luci-theme-rosy package/
 
 #luci-app-koolddns阿里解析
 git clone https://github.com/xrouterservice/luci-app-koolddns.git package/luci-app-koolddns
-svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/luci-app-ssr-plus
-#naiveproxy代理
-svn co https://github.com/fw876/helloworld/trunk/naiveproxy package/naiveproxy
 #赋予koolddns权限
 chmod 0755 package/luci-app-koolddns/root/etc/init.d/koolddns
 chmod 0755 package/luci-app-koolddns/root/usr/share/koolddns/aliddns
+
+svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/luci-app-ssr-plus
+#naiveproxy代理
+svn co https://github.com/fw876/helloworld/trunk/naiveproxy package/naiveproxy
 
 #添加smartdns
 svn co https://github.com/project-openwrt/openwrt/trunk/package/ntlf9t/smartdns package/smartdns
