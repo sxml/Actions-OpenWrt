6c868b289fbd0b31ebb958f872a9aeff31506dfb
sxml <59968675@qq.com>
2022-07-08 20:51:36 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #============================================================
-# # 2021-07-07
+# # 2021-07-08
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
