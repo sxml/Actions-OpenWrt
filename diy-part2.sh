c18285042ea257b4142e44bdc5cf5afb99d17167
sxml <59968675@qq.com>
2024-01-14 20:11:49 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -79,7 +79,7 @@ merge_package https://github.com/fw876/helloworld helloworld/redsocks2
 merge_package https://github.com/fw876/helloworld helloworld/gn
 
 merge_package https://github.com/kenzok8/openwrt-packages openwrt-packages/gost
-merge_package https://github.com/breakings/OpenWrt/trunk/general general/luci-app-gost
+merge_package https://github.com/breakings/OpenWrt/tree/main/general general/luci-app-gost
 
 #文件助手
 merge_package https://github.com/Lienol/openwrt-package openwrt-package/luci-app-fileassistant
