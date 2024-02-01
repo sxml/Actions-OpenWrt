0d41da331544137adf424434d671ccef2d061969
sxml <59968675@qq.com>
2024-02-01 21:39:45 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
 #============================================================
-# # 2024-01-18
+# # 2024-01-14
 #https://github.com/breakings/OpenWrt
-#https://github.com/binge8/op
 #============================================================
-merge_package(){
-    # 参数1是分支名,参数2是库地址。所有文件下载到openwrt/package/openwrt-packages路径。
-    # 同一个仓库下载多个文件夹直接在后面跟文件名或路径，空格分开。
-    trap 'rm -rf "$tmpdir"' EXIT
-    branch="$1" curl="$2" && shift 2
-    rootdir="$PWD"
-    localdir=package/openwrt-packages
-    [ -d "$localdir" ] || mkdir -p "$localdir"
-    tmpdir="$(mktemp -d)" || exit 1
-    git clone -b "$branch" --depth 1 --filter=blob:none --sparse "$curl" "$tmpdir"
-    cd "$tmpdir"
-    git sparse-checkout init --cone
-    git sparse-checkout set "$@"
-    mv -f "$@" "$rootdir"/"$localdir" && cd "$rootdir"
+echo "开始 DIY2 配置……"
+echo "========================="
+
+function merge_package(){
+    repo=`echo $1 | rev | cut -d'/' -f 1 | rev`
+    pkg=`echo $2 | rev | cut -d'/' -f 1 | rev`
+    # find package/ -follow -name $pkg -not -path "package/custom/*" | xargs -rt rm -rf
+    git clone --depth=1 --single-branch $1
+    mv $2 package/custom/
+    rm -rf $repo
 }
+function drop_package(){
+    find package/ -follow -name $1 -not -path "package/custom/*" | xargs -rt rm -rf
+}
+function merge_feed(){
+    if [ ! -d "feed/$1" ]; then
+        echo >> feeds.conf.default
+        echo "src-git $1 $2" >> feeds.conf.default
+    fi
+    ./scripts/feeds update $1
+    ./scripts/feeds install -a -p $1
+}
+rm -rf package/custom; mkdir package/custom
 
 #移除不用软件包
 #rm -rf package/lean/luci-app-dockerman
@@ -32,77 +39,79 @@ rm -fr feeds/luci/themes/luci-theme-design
 rm -rf feeds/packages/utils/prometheus-node-exporter-lua
 
 #添加smartdns
-merge_package master https://github.com/kenzok8/openwrt-packages openwrt-packages/smartdns
-merge_package master https://github.com/kenzok8/openwrt-packages openwrt-packages/luci-app-smartdns
+merge_package https://github.com/kenzok8/openwrt-packages openwrt-packages/smartdns
+merge_package https://github.com/kenzok8/openwrt-packages openwrt-packages/luci-app-smartdns
 #git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
 #git clone https://github.com/pymumu/openwrt-smartdns package/smartdns
 
 #添加passwall
-merge_package main https://github.com/xiaorouji/openwrt-passwall luci-app-passwall
-merge_package main https://github.com/xiaorouji/openwrt-passwall2 luci-app-passwall2
+merge_package https://github.com/xiaorouji/openwrt-passwall openwrt-passwall/luci-app-passwall
+merge_package https://github.com/xiaorouji/openwrt-passwall2 openwrt-passwall2/luci-app-passwall2
 
-merge_package main https://github.com/xiaorouji/openwrt-passwall-packages brook
-merge_package main https://github.com/xiaorouji/openwrt-passwall-packages chinadns-ng
-merge_package main https://github.com/xiaorouji/openwrt-passwall-packages tcping
-merge_package main https://github.com/xiaorouji/openwrt-passwall-packages trojan-go
-merge_package main https://github.com/xiaorouji/openwrt-passwall-packages trojan-plus
-merge_package main https://github.com/xiaorouji/openwrt-passwall-packages shadowsocks-rust
-merge_package main https://github.com/xiaorouji/openwrt-passwall-packages ssocks
-merge_package main https://github.com/xiaorouji/openwrt-passwall-packages dns2socks
-merge_package main https://github.com/xiaorouji/openwrt-passwall-packages ipt2socks
-merge_package main https://github.com/xiaorouji/openwrt-passwall-packages microsocks 
-merge_package main https://github.com/xiaorouji/openwrt-passwall-packages pdnsd-alt
-merge_package main https://github.com/xiaorouji/openwrt-passwall-packages v2ray-core
-merge_package main https://github.com/xiaorouji/openwrt-passwall-packages v2ray-plugin
-merge_package main https://github.com/xiaorouji/openwrt-passwall-packages simple-obfs
-merge_package main https://github.com/xiaorouji/openwrt-passwall-packages trojan
-merge_package main https://github.com/xiaorouji/openwrt-passwall-packages hysteria
-merge_package main https://github.com/xiaorouji/openwrt-passwall-packages sing-box
-merge_package main https://github.com/xiaorouji/openwrt-passwall-packages naiveproxy
+merge_package https://github.com/xiaorouji/openwrt-passwall-packages openwrt-passwall-packages/brook
+merge_package https://github.com/xiaorouji/openwrt-passwall-packages openwrt-passwall-packages/chinadns-ng
+merge_package https://github.com/xiaorouji/openwrt-passwall-packages openwrt-passwall-packages/tcping
+merge_package https://github.com/xiaorouji/openwrt-passwall-packages openwrt-passwall-packages/trojan-go
+merge_package https://github.com/xiaorouji/openwrt-passwall-packages openwrt-passwall-packages/trojan-plus
+merge_package https://github.com/xiaorouji/openwrt-passwall-packages openwrt-passwall-packages/shadowsocks-rust
+merge_package https://github.com/xiaorouji/openwrt-passwall-packages openwrt-passwall-packages/ssocks
+merge_package https://github.com/xiaorouji/openwrt-passwall-packages openwrt-passwall-packages/dns2socks
+merge_package https://github.com/xiaorouji/openwrt-passwall-packages openwrt-passwall-packages/ipt2socks
+merge_package https://github.com/xiaorouji/openwrt-passwall-packages openwrt-passwall-packages/microsocks 
+merge_package https://github.com/xiaorouji/openwrt-passwall-packages openwrt-passwall-packages/pdnsd-alt
+merge_package https://github.com/xiaorouji/openwrt-passwall-packages openwrt-passwall-packages/v2ray-core
+merge_package https://github.com/xiaorouji/openwrt-passwall-packages openwrt-passwall-packages/v2ray-plugin
+merge_package https://github.com/xiaorouji/openwrt-passwall-packages openwrt-passwall-packages/simple-obfs
+merge_package https://github.com/xiaorouji/openwrt-passwall-packages openwrt-passwall-packages/trojan
+merge_package https://github.com/xiaorouji/openwrt-passwall-packages openwrt-passwall-packages/hysteria
+merge_package https://github.com/xiaorouji/openwrt-passwall-packages openwrt-passwall-packages/sing-box
+merge_package https://github.com/xiaorouji/openwrt-passwall-packages openwrt-passwall-packages/naiveproxy
 
-merge_package master https://github.com/fw876/helloworld luci-app-ssr-plus
-merge_package master https://github.com/fw876/helloworld xray-core
-merge_package master https://github.com/fw876/helloworld xray-plugin
-merge_package master https://github.com/fw876/helloworld shadowsocksr-libev
-merge_package master https://github.com/fw876/helloworld lua-neturl
-merge_package master https://github.com/fw876/helloworld shadow-tls
-merge_package master https://github.com/fw876/helloworld tuic-client
-merge_package master https://github.com/fw876/helloworld dns2tcp
-merge_package master https://github.com/fw876/helloworld v2ray-geodata
-merge_package master https://github.com/fw876/helloworld redsocks2
-merge_package master https://github.com/fw876/helloworld gn
+merge_package https://github.com/fw876/helloworld helloworld/luci-app-ssr-plus
+merge_package https://github.com/fw876/helloworld helloworld/xray-core
+merge_package https://github.com/fw876/helloworld helloworld/xray-plugin
+merge_package https://github.com/fw876/helloworld helloworld/shadowsocksr-libev
+merge_package https://github.com/fw876/helloworld helloworld/lua-neturl
+merge_package https://github.com/fw876/helloworld helloworld/shadow-tls
+merge_package https://github.com/fw876/helloworld helloworld/tuic-client
+merge_package https://github.com/fw876/helloworld helloworld/dns2tcp
+merge_package https://github.com/fw876/helloworld helloworld/v2ray-geodata
+merge_package https://github.com/fw876/helloworld helloworld/redsocks2
+merge_package https://github.com/fw876/helloworld helloworld/gn
 
-merge_package main https://github.com/kenzok8/small-package sagernet-core gost luci-app-gost 
-#merge_package https://github.com/breakings/OpenWrt OpenWrt/general/luci-app-gost
+merge_package https://github.com/kenzok8/openwrt-packages openwrt-packages/gost
+merge_package https://github.com/breakings/OpenWrt OpenWrt/general/luci-app-gost
 
 #文件助手
-#merge_package main https://github.com/Lienol/openwrt-package openwrt-package/luci-app-fileassistant
-git clone https://github.com/sxml/luci-app-fileassistant.git package/luci-app-fileassistant
+merge_package https://github.com/Lienol/openwrt-package openwrt-package/luci-app-fileassistant
+#git clone https://github.com/sxml/luci-app-fileassistant.git package/luci-app-fileassistant
 
 #解析
-#merge_package main https://github.com/kenzok8/small-package luci-app-socat lua-maxminddb
-merge_package master https://github.com/jerrykuku/lua-maxminddb lua-maxminddb
+merge_package https://github.com/jerrykuku/lua-maxminddb lua-maxminddb
 
 #添加luci-app-amlogic
-merge_package main https://github.com/ophub/luci-app-amlogic luci-app-amlogic
+merge_package https://github.com/ophub/luci-app-amlogic luci-app-amlogic
 #修改晶晨宝盒默认配置
 # 1.设置OpenWrt 文件的下载仓库
-sed -i "s|https.*/OpenWrt|https://github.com/sxml/Actions-OpenWrt|g" package/openwrt-packages/luci-app-amlogic/root/etc/config/amlogic
+sed -i "s|https.*/OpenWrt|https://github.com/sxml/Actions-OpenWrt|g" package/custom/luci-app-amlogic/luci-app-amlogic/root/etc/config/amlogic
 # 2.设置 Releases 里 Tags 的关键字
-sed -i "s|ARMv8|ARMv8|g" package/openwrt-packages/luci-app-amlogic/root/etc/config/amlogic
+sed -i "s|ARMv8|ARMv8|g" package/custom/luci-app-amlogic/luci-app-amlogic/root/etc/config/amlogic
 # 3.设置 Releases 里 OpenWrt 文件的后缀
-sed -i "s|.img.gz|.img.gz|g" package/openwrt-packages/luci-app-amlogic/root/etc/config/amlogic
+sed -i "s|.img.gz|.img.gz|g" package/custom/luci-app-amlogic/luci-app-amlogic/root/etc/config/amlogic
 # 4.设置 OpenWrt 内核的下载路径
-#sed -i "s|opt/kernel|BuildARMv8|g" package/openwrt-packages/luci-app-amlogic/root/etc/config/amlogic
-sed -i "s|http.*/library|https://github.com/breakings/OpenWrt/tree/main/opt/kernel|g" package/openwrt-packages/luci-app-amlogic/root/etc/config/amlogic
+#sed -i "s|opt/kernel|BuildARMv8|g" package/custom/luci-app-amlogic/luci-app-amlogic/root/etc/config/amlogic
+sed -i "s|http.*/library|https://github.com/breakings/OpenWrt/tree/main/opt/kernel|g" package/custom/luci-app-amlogic/luci-app-amlogic/root/etc/config/amlogic
+
+#小猫
+#merge_package https://github.com/vernesong/OpenClash OpenClash/luci-app-openclash
+#参考
+#git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
 
 # themes 主题
-merge_package master https://github.com/Leo-Jo-My/luci-theme-opentomcat luci-theme-opentomcat
-merge_package main https://github.com/thinktip/luci-theme-neobird luci-theme-neobird
-merge_package main https://github.com/gngpp/luci-theme-design luci-theme-design
-merge_package master https://github.com/gngpp/luci-app-design-config luci-app-design-config
-#参考
-#git clone --depth=1 -b master https://github.com/Leo-Jo-My/luci-theme-opentomcat package/openwrt-packages/luci-theme-opentomcat
+merge_package https://github.com/Leo-Jo-My/luci-theme-opentomcat luci-theme-opentomcat
+merge_package https://github.com/thinktip/luci-theme-neobird luci-theme-neobird
+merge_package https://github.com/gngpp/luci-theme-design luci-theme-design
+merge_package https://github.com/gngpp/luci-app-design-config luci-app-design-config
 
 #修改makefile
 find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/include\ \.\.\/\.\.\/luci\.mk/include \$(TOPDIR)\/feeds\/luci\/luci\.mk/g' {}
@@ -115,3 +124,6 @@ sed -i 's/TARGET_CFLAGS += -DHAVE_MAP_SYNC/TARGET_CFLAGS += -DHAVE_MAP_SYNC -D_L
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
+
+echo "========================="
+echo " DIY2 配置完成……"
