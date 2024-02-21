45a4e8298f6e69443672642d235be0d9feed24d6
sxml <59968675@qq.com>
2024-02-21 12:53:15 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -101,17 +101,9 @@ git clone --depth=1 https://github.com/gngpp/luci-app-design-config.git package/
 # 临时修复xfsprogs
 sed -i 's|TARGET_CFLAGS += -DHAVE_MAP_SYNC.*|TARGET_CFLAGS += -DHAVE_MAP_SYNC $(if $(CONFIG_USE_MUSL),-D_LARGEFILE64_SOURCE)|' feeds/packages/utils/xfsprogs/Makefile
 
-# cryptsetup
-#sed -i 's/PKG_VERSION:=.*/PKG_VERSION:=2.6.1/g' feeds/packages/utils/cryptsetup/Makefile
-#sed -i 's/PKG_HASH:=.*/PKG_HASH:=410ded65a1072ab9c8e41added37b9729c087fef4d2db02bb4ef529ad6da4693/g' feeds/packages/utils/cryptsetup/Makefile
-#sed -i '78i\TARGET_CFLAGS += -D_LARGEFILE64_SOURCE\' feeds/packages/utils/cryptsetup/Makefile
-
 # perl
-rm -rf feeds/packages/lang/perl
-cp -rf $GITHUB_WORKSPACE/general/perl feeds/packages/lang
-
-# shadowsocks-rust
-#cp -rf $GITHUB_WORKSPACE/general/shadowsocks-rust package/shadowsocks-rust
+#rm -rf feeds/packages/lang/perl
+#cp -rf $GITHUB_WORKSPACE/general/perl feeds/packages/lang
 
 # curl
 sed -i 's/PKG_VERSION:=.*/PKG_VERSION:=8.5.0/g' feeds/packages/net/curl/Makefile
@@ -120,10 +112,10 @@ sed -i 's/PKG_RELEASE:=.*/PKG_RELEASE:=1/g' feeds/packages/net/curl/Makefile
 #rm -rf feeds/packages/net/curl
 
 # 修改libxml2的Makefile，启用shared
-sed -i '/HOST_CONFIGURE_ARGS/ s/--disable-shared/--enable-shared/' feeds/packages/libs/libxml2/Makefile
+#sed -i '/HOST_CONFIGURE_ARGS/ s/--disable-shared/--enable-shared/' feeds/packages/libs/libxml2/Makefile
 # 修改libxslt的Makefile，指定libxml的库路径和头文件路径
-sed -i '/HOST_CONFIGURE_ARGS/ a\--with-libxml-libs-prefix=$(STAGING_DIR_HOSTPKG)/lib' feeds/packages/libs/libxslt/Makefile
-sed -i '/HOST_CONFIGURE_ARGS/ a\--with-libxml-include-prefix=$(STAGING_DIR_HOSTPKG)/include/libxml2/' feeds/packages/libs/libxslt/Makefile
+#sed -i '/HOST_CONFIGURE_ARGS/ a\--with-libxml-libs-prefix=$(STAGING_DIR_HOSTPKG)/lib' feeds/packages/libs/libxslt/Makefile
+#sed -i '/HOST_CONFIGURE_ARGS/ a\--with-libxml-include-prefix=$(STAGING_DIR_HOSTPKG)/include/libxml2/' feeds/packages/libs/libxslt/Makefile
 # 修改xsltproc的Makefile，链接libxml2库
 #sed -i 's|LIBXML_LIBS =|LIBXML_LIBS = -lxml2 -L$(STAGING_DIR_HOSTPKG)/lib|' build_dir/hostpkg/libxslt-1.1.34/xsltproc/Makefile
 
