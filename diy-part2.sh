730f084242c6da39b7ba4c61f693090b0cdc7011
sxml <59968675@qq.com>
2024-10-23 06:35:45 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -118,8 +118,8 @@ cp -rf luci-app-ddns-go/luci-app-ddns-go package/luci-app-ddns-go
 git clone --depth=1 https://github.com/sxml/luci-app-filebrowser.git package/luci-app-filebrowser
 
 #lrzsz
-rm -rf feeds/packages/utils/lrzsz
-cp -rf $GITHUB_WORKSPACE/general/lrzsz feeds/packages/utils
+#rm -rf feeds/packages/utils/lrzsz
+#cp -rf $GITHUB_WORKSPACE/general/lrzsz feeds/packages/utils
 
 #修改makefile
 find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/include\ \.\.\/\.\.\/luci\.mk/include \$(TOPDIR)\/feeds\/luci\/luci\.mk/g' {}
