3fd4623ec8f9c800be65b7220974aa285e80a368
sxml <59968675@qq.com>
2024-12-16 06:31:35 +0800

    Update 1806diy-part2.sh

--- aarch64/Lede1806/1806diy-part2.sh
+++ aarch64/Lede1806/1806diy-part2.sh
@@ -137,7 +137,7 @@ popd
 
 #libcryptopp 编译问题
 # 在 Makefile 中显式指定编译器
-sed -i '/include $(INCLUDE_DIR)\/package.mk/a TARGET_CC:=aarch64-openwrt-linux-musl-gcc\nTARGET_CXX:=aarch64-openwrt-linux-musl-g++' package/lean/libcryptopp/Makefile
+#sed -i '/include $(INCLUDE_DIR)\/package.mk/a TARGET_CC:=aarch64-openwrt-linux-musl-gcc\nTARGET_CXX:=aarch64-openwrt-linux-musl-g++' package/lean/libcryptopp/Makefile
 # 清理 libcryptopp 的缓存
 #make package/lean/libcryptopp/clean
 
