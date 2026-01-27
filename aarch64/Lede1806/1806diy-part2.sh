76c7ebe992fdc941fdea0ffb3d452964af58ae73
sxml <59968675@qq.com>
2026-01-28 01:01:28 +0800

    Update 1806diy-part2.sh

--- aarch64/Lede1806/1806diy-part2.sh
+++ aarch64/Lede1806/1806diy-part2.sh
@@ -54,7 +54,7 @@ cp -rf small-package/ssocks package/ssocks
 cp -rf small-package/pdnsd-alt package/pdnsd-alt
 cp -rf small-package/trojan-go package/trojan-go
 cp -rf small-package/dns2tcp package/dns2tcp
-cp -rf small-package/v2ray-geoview package/v2ray-geoview
+# cp -rf small-package/v2ray-geoview package/v2ray-geoview
 rm -rf small-package
 
 # 克隆 fw876 仓库
@@ -62,7 +62,7 @@ git clone --depth=1 -b main https://github.com/fw876/helloworld.git
 #cp -rf helloworld/luci-app-ssr-plus package/luci-app-ssr-plus
 cp -rf helloworld/xray-core package/xray-core
 cp -rf helloworld/xray-plugin package/xray-plugin
-cp -rf helloworld/shadowsocks-libev package/shadowsocks-libev
+# cp -rf helloworld/shadowsocks-libev package/shadowsocks-libev
 cp -rf helloworld/shadowsocks-rust package/shadowsocks-rust
 #cp -rf helloworld/shadowsocksr-libev package/shadowsocksr-libev
 cp -rf helloworld/v2ray-plugin package/v2ray-plugin
